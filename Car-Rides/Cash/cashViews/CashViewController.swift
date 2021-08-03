0e58cfd24d43243b1d8e24e336bed0d7e2389c54
Emmanuel Obuh <81360481+emmanuelroyal@users.noreply.github.com>
2021-08-03 01:04:25 +0100

    feat: added map

--- Car-Rides/Cash/cashViews/CashViewController.swift
+++ Car-Rides/Cash/cashViews/CashViewController.swift
@@ -6,24 +6,129 @@
 //
 
 import UIKit
+import MapKit
+import FABButton
 
-class CashViewController: UIViewController {
+class CashViewController: UIViewController, CLLocationManagerDelegate, FABSecondaryButtonDelegate {
+    func secondaryActionForButton(_ action: @escaping () -> ()) {
+        <#code#>
+    }
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    private let locationManager = CLLocationManager()
+    private let mapTypeButton = FABView(buttonImage: UIImage(named: "earth"))
+    
+
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureMapTypeButton()
 
-        // Do any additional setup after loading the view.
+      
+    }
+    private func configureMapTypeButton() {
+        mapTypeButton.delegate = self
+     
+        mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "map")!, labelTitle: "Standard", action: {
+            self.map.mapType = .mutedStandard
+        })
+            
+       mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "satellite")!, labelTitle: "Satellite", action: {
+           self.map.mapType = .satellite
+       })
+            
+       mapTypeButton.addSecondaryButtonWith(image: UIImage(named: "hybrid")!, labelTitle: "Hybrid", action: {
+           self.map.mapType = .hybrid
+       })
+            
+       mapTypeButton.setFABButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func checkLocationServices() {
+        guard CLLocationManager.locationServicesEnabled() else {
+            // Here we must tell user how to turn on location on device
+            return
+        }
+            
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        checkAuthorizationForLocation()
     }
-    */
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkAuthorizationForLocation()
+       }
+       
+       
+       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+                let coordinateRegion = MKCoordinateRegion.init(center: location.coordinate,
+                                                               latitudinalMeters: rangeInMeters,
+                                                               longitudinalMeters: rangeInMeters)
+                
+            map.setRegion(coordinateRegion, animated: true)
+          
+       }
+    
+    private func checkAuthorizationForLocation() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            map.showsUserLocation = true
+            centerViewOnUser()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // Here we must tell user how to turn on location on device
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+                // Here we must tell user that the app is not authorize to use location services
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    private let rangeInMeters: Double = 10000
+
+    private func centerViewOnUser() {
+        guard let location = locationManager.location?.coordinate else { return }
+        
+        let coordinateRegion = MKCoordinateRegion.init(center: location,
+                                                       latitudinalMeters: rangeInMeters,
+                                                       longitudinalMeters: rangeInMeters)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    private let pointer = UIImageView(image: UIImage(systemName: "mappin"))
+    private func layoutUI() {
+        view.addSubview(mapTypeButton)
+        pointer.translatesAutoresizingMaskIntoConstraints = false
+        pointer.tintColor = .red
+        view.addSubview(pointer)
+        
+        NSLayoutConstraint.activate([
+            mapTypeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            mapTypeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            pointer.centerYAnchor.constraint(equalTo: map.centerYAnchor, constant: -14.5),
+            pointer.centerXAnchor.constraint(equalTo: map.centerXAnchor),
+            pointer.widthAnchor.constraint(equalToConstant: 27),
+            pointer.heightAnchor.constraint(equalToConstant: 29)
+       ])
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let coordinates = mapView.centerCoordinate
+        return CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    
+
+    
+    
 
 }
--- Car-Rides/PromoCodes/PromoCodesViews/PromoCodesViewController.swift
+++ Car-Rides/PromoCodes/PromoCodesViews/PromoCodesViewController.swift
@@ -12,7 +12,7 @@ class PromoCodesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
